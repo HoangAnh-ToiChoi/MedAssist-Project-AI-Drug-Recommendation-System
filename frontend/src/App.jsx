44bee6987548920c65bd9fb6fc1297b9e1076280
Đăng Khoa Phan Phạm <phankhoa0824@gmail.com>
2026-05-22 17:07:59 +0700

    CHCKNSPC-56 KHOA tao UI hien thi ket qua goi y thuoc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Register from './pages/Register';
 import OtpVerification from './components/auth/OtpVerification'; // Sửa đúng đường dẫn
 import SymptomInput from './pages/SymptomInput';
 import Dashboard from './pages/Dashboard';
+import DrugSuggestion from './pages/DrugSuggestion';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Route path="/verify-otp" element={<OtpVerification />} />
         <Route path="/symptoms" element={<SymptomInput />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/suggestions" element={<DrugSuggestion />} />
       </Routes>
     </BrowserRouter>
   );
